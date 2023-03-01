b721aa93d5a284636189e54645516af5613fed3b
surahj <odebodeopeyemi@gmail.com>
2023-03-01 01:34:51 +0100

    create user model

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+const secret = process.env.JWT_SECRET;
+const encodedSecret = () => Buffer.from(secret).toString('base64');
+
+const crypto = require('crypto');
+
+// Generate a random secret string of 32 bytes
+const sec= crypto.randomBytes(32).toString('hex');
+
+console.log(sec);
 
 app.get('/', (req, res) => {
   res.send('status OK');
---
+++ models/user.js
@@ -0,0 +1,60 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const secret = process.env.JWT_SECRET;
+const encodedSecret = Buffer.from(secret).toString('base64');
+
+
+const userSchema = new mongoose.Schema({
+  businessName: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
